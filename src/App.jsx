84376311365f45bb55ea530d2dfe67349c66cc4b
[wwatanabe331] <[wwatanabe331@gmail.com]>
2024-05-29 18:15:54 +0900

    アニメーションの追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,25 @@
-// d3.jsのDOM操作（.select(),など)を使わずに作成
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
-import * as d3 from "d3";
+import { scaleLinear, scaleOrdinal } from "d3-scale";
+import { schemeCategory10 } from "d3-scale-chromatic";
+import { extent } from "d3-array";
+import "./styles.css";
 
-const Chart = ({ data }) => {
+const Chart = ({ data, xAxis, yAxis }) => {
   const leftMargin = 100;
   const rightMargin = 200;
   const bottomMargin = 100;
-  const contentWidth = 400;
-  const contentHeight = 400;
-  const color = d3.scaleOrdinal(d3.schemeCategory10);
+  const contentWidth = 500;
+  const contentHeight = 500;
+  const color = scaleOrdinal(schemeCategory10);
 
-  const xScale = d3
-    .scaleLinear()
-    .domain([
-      d3.min(data, (x) => x.sepalLength),
-      d3.max(data, (x) => x.sepalLength),
-    ])
+  const xScale = scaleLinear()
+    .domain(extent(data, (d) => d[xAxis]))
     .range([0, contentWidth])
     .nice();
 
-  const yScale = d3
-    .scaleLinear()
-    .domain([
-      d3.min(data, (x) => x.sepalWidth),
-      d3.max(data, (x) => x.sepalWidth),
-    ])
+  const yScale = scaleLinear()
+    .domain(extent(data, (d) => d[yAxis]))
     .range([contentHeight, 0])
     .nice();
 
@@ -35,84 +29,131 @@ const Chart = ({ data }) => {
   return (
     <svg width={svgWidth} height={svgHeight}>
       <g transform={`translate(${leftMargin}, ${bottomMargin / 2})`}>
-        {/* x軸 */}
+        {/* X軸 */}
         <g transform={`translate(0, ${contentHeight})`}>
+          {/* <line x1={0} x2={contentWidth} stroke="black" /> */}
+          <line x2={contentWidth} stroke="black" />
           {xScale.ticks().map((tick, i) => (
             <g key={i} transform={`translate(${xScale(tick)}, 0)`}>
-              {/* 値の横の線 */}
               <line y2={5} stroke="black" />
-              {/* emとは */}
-              <text y={5} dy="1em" textAnchor="middle">
+              <text y={20} textAnchor="middle">
                 {tick}
               </text>
             </g>
           ))}
-          {/* 軸の線 */}
-          <line x2={contentWidth} stroke="black" />
+          <text
+            x={contentWidth / 2}
+            y={bottomMargin / 2.5}
+            textAnchor="middle"
+            className="axis-label"
+          >
+            {xAxis}
+          </text>
         </g>
-        {/* text表示されない問題 */}
-        <text
-          transform={`translate(${contentWidth / 2},${
-            contentHeight + bottomMargin / 2
-          })`}
-          text-anchor="middle"
-        >
-          sepal Length
-        </text>
 
-        {/* y軸 */}
+        {/* Y軸 */}
         <g>
+          {/* <line y1={0} y2={contentHeight} stroke="black" /> */}
+          <line y2={contentHeight} stroke="black" />
           {yScale.ticks().map((tick, i) => (
             <g key={i} transform={`translate(0, ${yScale(tick)})`}>
               <line x2={-5} stroke="black" />
-              <text x={-8} dy="0.3em" textAnchor="end">
+              <text x={-8} y={5} textAnchor="end">
                 {tick}
               </text>
             </g>
           ))}
-          <line y2={contentHeight} stroke="black" />
+          <text
+            transform="rotate(-90)"
+            x={-contentHeight / 2}
+            y={-leftMargin / 3 - 10}
+            textAnchor="middle"
+            className="axis-label"
+          >
+            {yAxis}
+          </text>
         </g>
-
-        {/* y軸のラベル */}
-        <text
-          transform="rotate(-90)"
-          x={-contentHeight / 2}
-          y={-leftMargin / 3 - 10}
-          textAnchor="middle"
-        >
-          Sepal Width
-        </text>
 
         {/* データポイント */}
         {data.map((d, i) => (
           <circle
             key={i}
-            cx={xScale(d.sepalLength)}
-            cy={yScale(d.sepalWidth)}
+            cx={xScale(d[xAxis])}
+            cy={yScale(d[yAxis])}
             r={5}
             fill={color(d.species)}
+            // アニメーションのためのクラス
+            className="circle"
           />
         ))}
+      </g>
 
-        {/* 凡例 */}
-        {/* 元々xはleftMargin足されている */}
-        <g transform={`translate(${contentWidth + 20}, 20)`}>
-          {Array.from(new Set(data.map((d) => d.species))).map((species, i) => (
-            <g key={i} transform={`translate(0, ${i * 20})`}>
-              <rect width={10} height={10} fill={color(species)} />
-              <text x={20} y={10} textAnchor="start">
-                {species}
-              </text>
-            </g>
-          ))}
-        </g>
+      {/* 凡例 */}
+      <g transform={`translate(${leftMargin + contentWidth + 20}, 20)`}>
+        {Array.from(new Set(data.map((d) => d.species))).map((species, i) => (
+          <g key={i} transform={`translate(0, ${i * 20})`}>
+            <rect width={10} height={10} fill={color(species)} />
+            <text x={20} y={10} textAnchor="start">
+              {species}
+            </text>
+          </g>
+        ))}
       </g>
     </svg>
   );
 };
 
-fetch("https://s3-us-west-2.amazonaws.com/s.cdpn.io/2004014/iris.json")
-  .then((response) => response.json())
-  .then((data) => {
-    ReactDOM.render(<Chart data={data} />, document.getElementById("root"));
-  });
+const App = () => {
+  // 初期値
+  const [xAxis, setXAxis] = useState("sepalLength");
+  const [yAxis, setYAxis] = useState("sepalWidth");
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://s3-us-west-2.amazonaws.com/s.cdpn.io/2004014/iris.json")
+      .then((response) => response.json())
+      .then((data) => setData(data));
+  }, []);
+
+  // xAxisを変更するためのイベントハンドラ
+  const handleXAxisChange = (e) => {
+    setXAxis(e.target.value);
+  };
+
+  const handleYAxisChange = (e) => {
+    setYAxis(e.target.value);
+  };
+
+  return (
+    <div>
+      <h1>Scatter Plot of Iris Flower Dataset</h1>
+      <div>
+        <label>
+          Horizontal Axis :
+          <select value={xAxis} onChange={handleXAxisChange}>
+            <option value="sepalLength">Sepal Length</option>
+            <option value="sepalWidth">Sepal Width</option>
+            <option value="petalLength">Petal Length</option>
+            <option value="petalWidth">Petal Width</option>
+          </select>
+        </label>
+      </div>
+
+      <div>
+        <label>
+          Vertical Axis :
+          <select value={yAxis} onChange={handleYAxisChange}>
+            <option value="sepalLength">Sepal Length</option>
+            <option value="sepalWidth">Sepal Width</option>
+            <option value="petalLength">Petal Length</option>
+            <option value="petalWidth">Petal Width</option>
+          </select>
+        </label>
+      </div>
+
+      <Chart data={data} xAxis={xAxis} yAxis={yAxis} />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
